020d921082c99be2d56a0b248c15fd97f122ecea
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-23 10:20:55 +0530

    [Anshuman]Added: UC-1 Employee Attendance

---
+++ EmployeeWageComputation/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        const int IS_FULLTIME = 1;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_FULLTIME)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.Attendance();
         }
     }
 }
