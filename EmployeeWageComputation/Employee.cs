ac719da66d751a887ae48d14db487a29a628d550
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-23 20:39:36 +0530

    [Anshuman] Added: Adding Employee Monthly Wage

--- EmployeeWageComputation/Employee.cs
+++ EmployeeWageComputation/Employee.cs
@@ -8,29 +8,34 @@ namespace EmployeeWageComputation
 {
     internal class EmployeeWage
     {
-        const int IS_FULLTIME = 1, IS_PARTTIME = 2, EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0, empWage = 0;
-        public void CalulateWage()
+        const int IS_FULLTIME = 1, IS_PARTTIME = 2, EMP_RATE_PER_HOUR = 20, WORKING_DAY = 20;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        public void CalulateMonthlyWage()
         {
-            Random random = new Random();
-            int Check = random.Next(0, 3);
-            switch (Check)
+            for (int day = 0; day < WORKING_DAY; day++)
             {
-                case IS_FULLTIME: 
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Present for Full-Time Today");
-                    break;
-                case IS_PARTTIME: 
-                    empHrs = 4;
-                    Console.WriteLine("Employee is Present for Part-Time Today");
-                    break;
-                default:          
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent for Today");
-                    break;
+                Random random = new Random();
+                int Check = random.Next(0, 3);
+                switch (Check)
+                {
+                    case IS_FULLTIME:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is Present for Full-Time Today");
+                        break;
+                    case IS_PARTTIME:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is Present for Part-Time Today");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is Absent for Today");
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Today's Wage :" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage :" + empWage);
+            Console.WriteLine("Total Employee Wage For Month : " + totalEmpWage);
         }
     }
 }
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             EmployeeWage employeeWage = new EmployeeWage();
-            employeeWage.CalulateWage();
+            employeeWage.CalulateMonthlyWage();
         }
     }
 }
