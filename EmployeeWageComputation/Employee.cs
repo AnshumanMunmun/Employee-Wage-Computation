17256d4e41e5245c01adb6acdbbb43a2f2af7a49
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-23 20:00:52 +0530

    [Anshuman] Added: Emp-WageComputation using SwitchCase

--- EmployeeWageComputation/Employee.cs
+++ EmployeeWageComputation/Employee.cs
@@ -8,26 +8,26 @@ namespace EmployeeWageComputation
 {
     internal class EmployeeWage
     {
-        const int IS_FULLTIME = 1, IS_HALFTIME = 2, EMP_RATE_PER_HOUR = 20;
+        const int IS_FULLTIME = 1, IS_PARTTIME = 2, EMP_RATE_PER_HOUR = 20;
         int empHrs = 0, empWage = 0;
-        public void PartTime()
+        public void CalulateWage()
         {
             Random random = new Random();
-            int check = random.Next(0, 3);
-            if (check == IS_FULLTIME)
+            int Check = random.Next(0, 3);
+            switch (Check)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is Present for Fulltime");
-            }
-            else if (check == IS_HALFTIME)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is Present for Halfime");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_FULLTIME: 
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Present for Full-Time Today");
+                    break;
+                case IS_PARTTIME: 
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Present for Part-Time Today");
+                    break;
+                default:          
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent for Today");
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage :" + empWage);
