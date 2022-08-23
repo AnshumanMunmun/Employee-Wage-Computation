4daf5c54c187e1f222a2526e200ba9fa1c26b3c5
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-23 19:24:35 +0530

    [Anshuman] Added: Adding Part-Time Wage

--- EmployeeWageComputation/Employee.cs
+++ EmployeeWageComputation/Employee.cs
@@ -8,16 +8,21 @@ namespace EmployeeWageComputation
 {
     internal class EmployeeWage
     {
-        const int IS_FULLTIME = 1, EMP_RATE_PER_HOUR = 20;
+        const int IS_FULLTIME = 1, IS_HALFTIME = 2, EMP_RATE_PER_HOUR = 20;
         int empHrs = 0, empWage = 0;
-        public void EmpWage()
+        public void PartTime()
         {
             Random random = new Random();
-            int check = random.Next(0, 2);
+            int check = random.Next(0, 3);
             if (check == IS_FULLTIME)
             {
                 empHrs = 8;
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present for Fulltime");
+            }
+            else if (check == IS_HALFTIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is Present for Halfime");
             }
             else
             {
