498245ff567fb2c52dbb422c16a2f6ecf84e3d6f
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-23 21:26:06 +0530

    [Anshuman] Added: Calculating Total Work per hours or days

--- EmployeeWageComputation/Employee.cs
+++ EmployeeWageComputation/Employee.cs
@@ -8,12 +8,13 @@ namespace EmployeeWageComputation
 {
     internal class EmployeeWage
     {
-        const int IS_FULLTIME = 1, IS_PARTTIME = 2, EMP_RATE_PER_HOUR = 20, WORKING_DAY = 20;
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
-        public void CalulateMonthlyWage()
+        const int IS_FULLTIME = 1, IS_PARTTIME = 2, EMP_RATE_PER_HOUR = 20, WORKING_DAY = 20, MONTHLY_HRS = 100;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        public void TotalWorkingHrs()
         {
-            for (int day = 0; day < WORKING_DAY; day++)
+            while (totalEmpHrs <= MONTHLY_HRS && totalWorkingDays < WORKING_DAY )
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int Check = random.Next(0, 3);
                 switch (Check)
@@ -31,11 +32,11 @@ namespace EmployeeWageComputation
                         Console.WriteLine("Employee is Absent for Today");
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Today's Wage :" + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Employee Working Hrs:" + empHrs);
             }
-            Console.WriteLine("Total Employee Wage For Month : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
         }
     }
 }
